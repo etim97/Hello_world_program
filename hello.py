907636b8cb40f55510fecf3e8cfef28511fc2695
Etim97 <wisdomlds97@gmail.com>
2026-07-04 17:29:45 +0100

    Week01 Hello_world

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 print (" Hello World!")
 print()
-print( " My name is Wisdom, This is a simple Python code ")
+print( " My name is Wisdom Etim, This is a simple Python code That prints Hello World and my name. ")
